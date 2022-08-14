ca54020ac6b44721c5c6d5b72b8c6b2fcf63fab6
zu2 <3023910+zu2@users.noreply.github.com>
2022-08-14 22:43:25 +0900

    add some keywords.

--- src/background.js
+++ src/background.js
@@ -1,9 +1,50 @@
-'use strict'
-
 chrome.tabs.onUpdated.addListener((_tabId, _changeInfo, tab) => {
-	const s='(gclid|fbclid|fm|n_cid|utm_[a-z]+)=(.*?&|.*)';
-    if (tab.url.match(new RegExp('[?&#]'+s, 'g')))
+	const q=[
+			'(g|i|fb|n_)clid',
+			'(utm|stm)_[a-z]+',
+			'bdad',
+			'bdactcd',
+			'bdl',
+			'bdmlc',
+			'btag',
+			'CMP',
+			'cx_artPos',
+			'cx_testId',
+			'cx_testVariant',
+			'dicbo',
+			'fm',
+			'igshid',
+			'internal',
+			'i_cid',
+			'i_cidinternal',
+			'mc_(cid|eid)',
+			'mkt_tok',
+			'_hs(enc|mi)',
+			'mc_[ce]id',
+			'n_cid',
+			'otc',
+			'oly_(anon|enc)_id',
+			'rb_clickid',
+			'rct',
+			'soc_(src|trk)',
+			'ST',
+			'td',
+			'wickedid',
+			'xadid',
+			'yclid',
+			'_openstat',
+			'__scale',
+			'_sh'
+			];
+	const s= '(' + q.join('\|') + ')=(.*?&|.*)';
+	console.log(q);
+	console.log(s);
+    if (tab.url.match(new RegExp('[?&#]'+s, 'g'))){
         chrome.tabs.update(tab.id,
-            { url: tab.url.replace(new RegExp(s, 'g'), '')
-						  .replace(new RegExp('[\?\&]$','g'),'') })
-})
+            { url: tab.url.replace(new RegExp(s, 'g'), '') });
+	}
+    if (tab.url.match(new RegExp('[?&#]$','g'))){
+        chrome.tabs.update(tab.id,
+            { url: tab.url.replace(new RegExp('[\?\&#]$','g'),'') });
+	}
+});
